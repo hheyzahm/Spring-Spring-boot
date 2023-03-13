fb13b35e8fa33e7d406b5489ba9442a73bdd8464
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-13 15:52:19 +0500

    Initial Commit 13 March - 2023

---
+++ src/main/java/springFrameworkSpringBoot/SpringFrameworkAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package springFrameworkSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springFrameworkSpringBoot/SpringFrameworkAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springFrameworkSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
